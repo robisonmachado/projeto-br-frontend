745a862f1ad34376f26b772448da8ccebc051947
Robison Pereira Machado <robison.machado.prestador@prf.gov.br>
2023-01-03 22:18:23 -0300

    início da página dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './pages/login/login.component';
+import { LoginModule } from './pages/login/login.module';
 
 const routes: Routes = [
   {
     path: '',
+    component: LoginComponent,
+
+  },
+  {
+    path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), LoginModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  cpf?: string
+  senha?: string
+
+  constructor(private router: Router){
+
+  }
+
+  entrar(){
+    console.log('entrar...', this.cpf, this.senha);
+
+    const loginOK = this.cpf == '1' && this.senha == '2';
+
+    if(loginOK){
+      this.router.navigate(['dashboard'])
+    }
+
+  }
+
+  esqueciSenha(){
+    console.log('esqueci a senha...');
+  }
 }
